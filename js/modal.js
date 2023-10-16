ebea3555ad72ab0486b64b0363b9791a876d2a7a
VladKorniiko <korniyko1911@gmail.com>
2023-10-16 16:41:23 +0300

    dz6

--- js/modal.js
+++ js/modal.js
@@ -8,3 +8,12 @@ document
   .addEventListener('click', function () {
     document.getElementById('modal-order').classList.remove('is-open');
   });
+
+document.getElementById('mob-menu-btn').addEventListener('click', function () {
+  document.getElementById('mob-menu').classList.add('is-open');
+});
+document
+  .getElementById('mob-menu-close')
+  .addEventListener('click', function () {
+    document.getElementById('mob-menu').classList.remove('is-open');
+  });
